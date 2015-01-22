1bd35a659c86cbf67ffedd9ae2ee16a6f479dc58
Nestor Castillo <nestor.castillo@izea.com>
2015-01-21 20:37:05 -0500

    Progress from Dojo

--- lib/tile_calculator.rb
+++ lib/tile_calculator.rb
@@ -1,5 +1,17 @@
 require 'pry'
 
 class TileCalculator
+  def initialize(args)
+    @width = args[:width]
+    @length = args[:length]
+    @price = args[:price]
+  end
 
+  def total_cost
+    @width * @length * @price
+  end
+
+  def print_total_cost
+    "The total cost will be $#{total_cost.to_i}"
+  end
 end
--- spec/tile_calculator_spec.rb
+++ spec/tile_calculator_spec.rb
@@ -3,4 +3,30 @@ require 'pry'
 
 describe TileCalculator do
 
+  let(:new_tile) { TileCalculator.new({width: 10, length: 12, price: 1.50}) }
+  let(:cheap_tile) { TileCalculator.new({width: 2, length: 4, price: 2}) }
+
+  it "Can create a new object" do
+    expect(new_tile).to be_an_instance_of(TileCalculator)
+  end
+
+  it "must have an argument" do
+    expect { new_tile }.not_to raise_error
+  end
+
+  it "should return a total_cost" do
+    expect(new_tile.total_cost).to eq 180
+  end
+
+  it "should return the product of the arguments in total_cost" do
+    expect(cheap_tile.total_cost).to eq 16
+  end
+
+  it "should print the total cost as a string" do
+    expect(new_tile.print_total_cost).to eq "The total cost will be $180"
+  end
+
+  it "should print the total cost as calculated" do
+    expect(cheap_tile.print_total_cost).to eq "The total cost will be $16"
+  end
 end
